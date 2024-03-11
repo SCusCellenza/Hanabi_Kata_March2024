604ab1a954bc3519f69f3707966a1f23bfd8430d
Sven Cusseau <sven.cusseau@cellenza.com>
2024-03-11 10:37:45 +0100

    Add three tests to check mistakes and if the game is over and lost

---
+++ Hanabi_Kata_March2024/Game.cs
@@ -0,0 +1,32 @@
+﻿
+
+namespace Hanabi_Kata_March2024
+{
+    internal class Game
+    {
+
+        public int NumberOfMistakesMade { get; private set; }
+        public Game()
+        {
+            NumberOfMistakesMade = 0;
+        }
+
+
+        internal bool IsLost()
+        {
+            if (NumberOfMistakesMade == 3) return true;
+            return false;
+        }
+
+        internal bool IsOver()
+        {
+            if (IsLost()) return true;
+            return false;
+        }
+
+        internal void MistakeIsMade()
+        {
+            NumberOfMistakesMade++;
+        }
+    }
+}
---
+++ Hanabi_Kata_March2024/GameTest.cs
@@ -0,0 +1,51 @@
+namespace Hanabi_Kata_March2024
+{
+    public class GameTest
+    {
+        [Fact]
+        public void IfAMistakeIsMade_ARedTokenIsAddedInTheBox()
+        {
+            //Arrange
+            Game game = new Game();
+            int initialMistakes = game.NumberOfMistakesMade;
+
+            //Act
+            game.MistakeIsMade();
+
+            //Assert
+            Assert.Equal(1, game.NumberOfMistakesMade -  initialMistakes);
+        }
+
+        [Fact]
+        public void IfThreeMistakesAreMade_GameIsOverAndGameIsLost()
+        {
+            //Arrange
+            Game game = new Game();
+
+            //Act
+            game.MistakeIsMade();
+            game.MistakeIsMade();
+            game.MistakeIsMade();
+
+            //Assert
+            Assert.True(game.IsOver());
+            Assert.True(game.IsLost());
+            Assert.Equal(3, game.NumberOfMistakesMade);
+        }
+
+        [Fact]
+        public void IfLessThanThreeMistakesAreMade_GameIsNotOverAndNotLost()
+        {
+            //Arrange
+            Game game = new Game();
+
+            //Act
+            game.MistakeIsMade();
+
+            //Assert
+            Assert.False(game.IsOver());
+            Assert.False(game.IsLost());
+            Assert.NotEqual(3, game.NumberOfMistakesMade);
+        }
+    }
+}

ca5ac9763b9e7f96f284e9ca5d2431dc9eda7a25
Sven <sven.cusseau@cellenza.com>
2024-03-28 11:17:30 +0100

    Add different number of cards in hand depending of players number

--- Hanabi_Kata_March2024/GameFactory.cs
+++ Hanabi_Kata_March2024/GameFactory.cs
@@ -8,13 +8,34 @@ namespace Hanabi_Kata_March2024
 
         internal Game TryCreateGame(int numberOfPlayers)
         {
+            if (numberOfPlayers < 2 || numberOfPlayers > 5)
+            {
+                throw new System.ArgumentException("Number of players must be between 2 and 5");
+            }
             Deck cardDeck = new Deck();
+
+            DealHands(numberOfPlayers, cardDeck);
+
+            return new Game(cardDeck, Players);
+        }
+
+        private void DealHands(int numberOfPlayers, Deck cardDeck)
+        {
+            int numberOfCardsPerPlayer;
+
+            if (numberOfPlayers == 2 || numberOfPlayers == 3)
+            {
+                numberOfCardsPerPlayer = 5;
+            }
+            else
+            {
+                numberOfCardsPerPlayer = 4;
+            }
 
             for (int i = 0; i < numberOfPlayers; i++)
             {
-                Players.Add(new Player(cardDeck.DealHand(4)));
+                Players.Add(new Player(cardDeck.DealHand(numberOfCardsPerPlayer)));
             }
-            return new Game(cardDeck, Players);
         }
     }
 }
--- Hanabi_Kata_March2024/GameFactoryTest.cs
+++ Hanabi_Kata_March2024/GameFactoryTest.cs
@@ -32,5 +32,66 @@ namespace Hanabi_Kata_March2024
             //Assert
             Assert.Equal(numberOfPlayers, gameFactory.Players.Count());
         }
+
+        [Theory]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void WhenAGameFactoryIsCalledFor2or3Players_ThenEachPlayerReceives5Cards(int numberOfPlayers)
+        {
+            //Arrange
+            GameFactory gameFactory = new GameFactory();
+
+            //Act
+            Game game = gameFactory.TryCreateGame(numberOfPlayers);
+
+            //Assert
+            Assert.Equal(5, game.Players[0].hand.Count());
+            Assert.Equal(5, game.Players[1].hand.Count());
+        }
+
+        [Theory]
+        [InlineData(4)]
+        [InlineData(5)]
+        public void WhenAGameFactoryIsCalledFor4or5Players_ThenEachPlayerReceives4Cards(int numberOfPlayers)
+        {
+            //Arrange
+            GameFactory gameFactory = new GameFactory();
+
+            //Act
+            Game game = gameFactory.TryCreateGame(numberOfPlayers);
+
+            //Assert
+            Assert.Equal(4, game.Players[0].hand.Count());
+            Assert.Equal(4, game.Players[1].hand.Count());
+        }
+
+        [Fact]
+        public void WhenAGameFactoryIsCalledFor2Players_ThenDeckContains40Cards()
+        {
+            //Arrange
+            GameFactory gameFactory = new GameFactory();
+            int numberOfPlayers = 2;
+
+            //Act
+            Game game = gameFactory.TryCreateGame(numberOfPlayers);
+
+            //Assert
+            Assert.Equal(40, game.Deck.Count());
+        }
+
+        //[Theory]
+        //[InlineData(1)]
+        //[InlineData(6)]
+        //public void WhenAGameFactoryIsCalledForLessThan2PlayersOrMoreThan5Players_ThenErrorIsThrown(int numberOfPlayers)
+        //{
+        //    //Arrange
+        //    GameFactory gameFactory = new GameFactory();
+
+        //    //Act
+        //    Action act = () => gameFactory.TryCreateGame(numberOfPlayers);
+
+        //    //Assert
+        //    Assert.Throws<ArgumentException>(act);
+        //}
     }
 }
